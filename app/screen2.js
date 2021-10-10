84e1f795a59d1d3e8c18b39397ce42bbd8382ae1
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-10-10 14:41:43 +0530

    timer , image and 2 screen with all texts

--- app/screen2.js
+++ app/screen2.js
@@ -3,33 +3,27 @@
 import React, {Component} from 'react';
 //import CountDown from 'react-native-countdown-component';
 
+import Ctimer from './timer';
 
-import {
-  Text,
-  View,
-  StyleSheet,
-  ImageBackground,
-  Image,
-  StatusBar,
-  SafeAreaView,
-  Dimensions,
-  TouchableOpacity,
-  CountDown
-} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 export default class screen_2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      passcode: ['', '', '', ''],
-    };
-  }
-
   render() {
     return (
       <View style={styles2.container}>
-        <View style={styles2.txt1_Vw}>
-          <Text style={styles2.enter_text}>Maximum attempt reached</Text>
+        <View style={styles2.view1}>
+          <Ctimer />
+          <Text style={styles2.text1}>Maximum attempt reached</Text>
+          <View>
+            <Image
+              style={styles2.lockicon}
+              source={require('./assets/picture/lock.png')}
+            />
+          
+          </View>
+          <View style ={styles2.midview}>
+            <Text style = {styles2.text2}>To protect your information access has been locked for 1 minites. Come back later and try again</Text>
+          </View>
         </View>
       </View>
     );
@@ -40,18 +34,42 @@ const styles2 = StyleSheet.create({
     flex: 1,
   },
 
-  txt1_Vw: {
+  view1: {
+    marginTop : 29,
     Height: 178,
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  enter_text: {
+  text1: {
     fontFamily: 'roboto',
     fontSize: 25,
     color: '#B3C1D4',
     letterSpacing: -0.41,
     lineHeight: 40,
     marginTop: 25,
+  },
+
+  lockicon: {
+    height: 70,
+    width: 70,
+    marginTop : 220,
+  },
+  text2: {
+    padding : 30,
+    fontFamily: 'roboto',
+    fontSize: 16,
+    color: 'black',
+    letterSpacing: 0,
+    lineHeight: 25,
+    textAlign : 'center'
+  },
+
+  midview: {
+    padding: 10,
+    marginTop : 20,
+    Height: 178,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- app/timer.js
+++ app/timer.js
@@ -17,37 +17,22 @@ import {
   
 } from 'react-native';
 
-export default class c_downtimer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    
-    };
-  }
-
+export default class Ctimer extends Component {
+  
   render() {
     return (
       <View style={styles3.container}>
-        <View style={styles3.txt1_Vw}>
-            <CountDown 
-             until={60}
-             size={30}
-             onFinish={() => alert('Finished')}
-            //digitStyle={{backgroundColor: '#FFF'}}
-            //  digitTxtStyle={{color: '#1CC625'}}
-             timeToShow={['M', 'S']}
-             timeLabels={{m: 'MM', s: 'SS'}}
-           />
+        <View style={styles3.timerVw}>
+          <CountDown 
             
-            
-            {/* <CountDown
-            until={1}
-            onFinish={console.log('done')}
-            size = {20}
-            timeToShow = {['M','S']}
-            /> */}
-          
-          {/* <Text style={styles3.enter_text}>Maximum attempt reached</Text> */}
+            until={60}
+            size={28}
+            onFinish={() => alert('Finished')}
+            digitStyle={{backgroundColor: '#FFF'}}
+            digitTxtStyle={{color: 'black'}}
+           timeToShow={['M','S']}
+           timeLabels={{m: 'MM', s: 'SS'}}
+          />
         </View>
       </View>
     );
@@ -59,18 +44,22 @@ const styles3 = StyleSheet.create({
     flex: 1,
   },
 
-  txt1_Vw: {
-    Height: 178,
+  timerVw: {
+    Height: 150,
     alignItems: 'center',
     justifyContent: 'center',
+    alignItems : 'center',
+    marginTop : 200
+    
+    // flexDirection: "column"
   },
 
-  enter_text: {
-    fontFamily: 'roboto',
-    fontSize: 25,
-    color: '#B3C1D4',
-    letterSpacing: -0.41,
-    lineHeight: 40,
-    marginTop: 25,
-  },
+  // timerc: {
+  //   Height: 200,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   alignItems : 'center'
+  // },
+
+
 });
